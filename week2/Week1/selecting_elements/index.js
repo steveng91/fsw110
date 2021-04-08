28f698712af8b4b1d60c4a4615dccf447a9ba05b
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-04-08 13:49:04 -0700

    commit_update

---
+++ week2/Week1/selecting_elements/index.js
@@ -0,0 +1,5 @@
+console.log(document)
+var greeting = document.getElementById('myHeader')
+console.log(greeting)
+console.dir(greeting)
+document.getElementById('myHeader').innerHTML="Welcome to round 2"
