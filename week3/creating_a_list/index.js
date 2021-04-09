014118d324df9b083d2cf37f983de8aff2c3db65
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-04-08 17:59:39 -0700

    assignment_complete

--- week3/creating_a_list/index.js
+++ week3/creating_a_list/index.js
@@ -1,1 +1,13 @@
-document.getelementby
+for (var i = 0; i < 10; i ++) {
+    var newH = document.createElement("h1");
+    newH.textContent = "Hello World"
+    newH.style.color = "red"
+    document.body.appendChild(newH);
+}
+
+const names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"]
+for(var i = 0; i < names.length; i++){
+var nameList = document.createElement('li')
+nameList.textContent = names[i]
+document.body.appendChild(nameList)
+}
