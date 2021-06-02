695154a05443ec59a2df110ec1ba53a5908e6f54
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-06-02 09:37:42 -0700

    week-4-commit

---
+++ week4/calculatorJS/index.js
@@ -0,0 +1,12 @@
+var num1 = document.getElementsByClassName(num1)
+var num2 = document.getElementsByClassName(num2)
+function add(num1, num2){
+    return num1 + num2
+}
+function subtract(num1, num2){
+    return num1 - num2
+}
+function multiply(num1, num2){
+    return num1 * num2
+}
+document.getElementsByClassName('result')
--- week4/dj_js/index.js
+++ week4/dj_js/index.js
@@ -1,3 +1,52 @@
-var box1 = document.getElementById(box)
-box.addEventListener('load', function(){
+var el = document.getElementById('box')
+
+window.addEventListener('load', function(){
+    el.style.backgroundColor='black'
+})
+
+el.addEventListener('mouseover', function(){
+    el.style.backgroundColor='green'
+})
+
+el.addEventListener('mousedown', function(){
+    el.style.backgroundColor='yellow'
+})
+
+el.addEventListener('mouseup', function(){
+    el.style.backgroundColor='blue'
+})
+
+el.addEventListener('dblclick', function(){
+    el.style.backgroundColor='red'
+})
+window.addEventListener("scroll",function(){
+    el.style.backgroundColor='purple'
+})
+addEventListener("keydown",function(event){
+    if(event.keyCode ==87)
+    el.style.backgroundColor='white'
+})
+addEventListener("keydown",function(event){
+    if(event.keyCode ==66)
+    el.style.backgroundColor='black'
+})
+addEventListener("keydown",function(event){
+    if(event.keyCode ==71)
+    el.style.backgroundColor='green'
+})
+addEventListener("keydown",function(event){
+    if(event.keyCode ==89)
+    el.style.backgroundColor='yellow'
+})
+addEventListener("keydown",function(event){
+    if(event.keyCode ==66)
+    el.style.backgroundColor='blue'
+})
+addEventListener("keydown",function(event){
+    if(event.keyCode ==82)
+    el.style.backgroundColor='red'
+})
+addEventListener("keydown",function(event){
+    if(event.keyCode ==80)
+    el.style.backgroundColor='purple'
 })
