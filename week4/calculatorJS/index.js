fcf9eae73f63c898f094d445127f93232e9dfc60
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-06-03 14:50:52 -0700

    week-4-calculator

--- week4/calculatorJS/index.js
+++ week4/calculatorJS/index.js
@@ -1,12 +1,19 @@
-var num1 = document.getElementsByClassName(num1)
-var num2 = document.getElementsByClassName(num2)
 function add(num1, num2){
-    return num1 + num2
+    var a = document.getElementById("num1").value
+    var b = document.getElementById("num2").value
+    var added= +a + +b;
+    document.getElementById("equals1").textContent = added;
+    
 }
-function subtract(num1, num2){
-    return num1 - num2
+function subtract(num3, num4){
+    var c = document.getElementById("num3").value
+    var d = document.getElementById("num4").value
+    var subtracted = +c - +d
+    document.getElementById("equals2").textContent = subtracted
 }
-function multiply(num1, num2){
-    return num1 * num2
+function multiply(num5, num6){
+    var e = document.getElementById("num5").value
+    var f = document.getElementById("num6").value
+    var multiplied = +e * +f
+    document.getElementById("equals3").textContent = multiplied
 }
-document.getElementsByClassName('result')
