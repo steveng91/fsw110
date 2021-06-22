59e2be732b0c8264353d45e7ba8ccd9008d5afbb
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2021-06-22 16:57:19 -0700

    Week_5_update

--- week5/submit_events_part_2/index.js
+++ week5/submit_events_part_2/index.js
@@ -1,3 +1,20 @@
-$('input[type="checkbox"]').on('change', function(){
-    $('input[type="checkbox"]').not(this).prop('checked', false)
-})
+const form = document.myForm
+ form.addEventListener('submit',(event)=>{
+     event.reset()
+ })
+function displayAlert() {
+    var fname = form.fname.value; 
+    var lname = form.lname.value;
+    var age = form.age.value;
+    var question1 = form.decision.value;
+    var question2 = []
+    for (let i = 0; i < form.owned.length; i++){
+        if(form.owned[i].checked){
+            question2.push(form.owned[i].value)
+        }
+    }
+    var question3 = form.sat.value;
+
+    alert('First Name: ' + fname + '\n Last Name: ' + lname + '\n Age:' + age + "\n past owner: " + question1 + "\n type owned: " + question2  + "\n experience: "+ question3);
+    document.getElementByName('myForm');
+}
---
+++ week6/event-workbook/click/app.js
@@ -0,0 +1,5 @@
+// make the box disapear when the user clicks it
+var box = document.querySelector('red-box');
+box.addEventListener('click', function(event){
+  box.style.display = 'none';
+})
